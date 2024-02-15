268ac33705b1a4cf2f9f89f3d7c786abc2ec8daf
Kacper <pogromcakaszy@gmail.com>
2024-02-15 14:22:11 +0100

    0.2.0 Working Test Delay

--- Delayion/src/screens/Home.js
+++ Delayion/src/screens/Home.js
@@ -1,127 +1,121 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import {
-    StyleSheet,
-    Text,
-    View,
-    TouchableOpacity,
-    ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import axios from 'axios';
 
 const apiKey = '5c188f0c-0b40-44ee-9ae1-96b818de8fa5';
-//const stopId = 8227;
-//const apiUrl = `https://ckan2.multimediagdansk.pl/departures?stopId=${stopId}`;
-//const apiUrl = `http://ckan2.multimediagdansk.pl/delays?stopId=${stopId}`;
-const headsign = "Oliwa";
-const apiUrl = `https://ckan2.multimediagdansk.pl/departures?headsign=${headsign}`;
+const stopId = 31430;
+const apiUrl = `http://ckan2.multimediagdansk.pl/delays?stopId=${stopId}`;
 
 const headers = {
-    'Authorization': `Bearer ${apiKey}`,
-    'Content-Type': 'application/json',
-    
+  'Authorization': `Bearer ${apiKey}`,
+  'Content-Type': 'application/json',
 };
 
 const Home = () => {
+    const [, forceUpdate] = useReducer(x => x + 1, 0);
 
-    const [departures, setDepartures] = useState(departures);
-
+    const [delays, setDelays] = useState([]);
+  
     useEffect(() => {
-        
-            fetchData();
-            const timeout = setInterval(() => {
-                fetchData();
-            }, 20 * 1000);
-    
-            return () => {
-                clearInterval(timeout);
-            };
+      fetchData();
+      const intervalId = setInterval(fetchData, 20 * 1000);
+  
+      return () => {
+        clearInterval(intervalId);
+      };
     }, []);
-
-    const fetchData = (async() => {
-        axios.get(apiUrl, { headers })
-            .then(response => {
-                console.log('Status Code:', response.status);
-                console.log('Response Data:', response.data);
-
-                setDepartures(response.data.departures);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-    });
-
+  
+    const fetchData = async () => {
+        try {
+          const response = await axios.get(apiUrl, { headers });
+          console.log('Status Code:', response.status);
+          console.log('Response Data:', response.data);
+      
+          const fetchedDelays = response.data.delay || [];
+          console.log('Fetched Delays:', fetchedDelays);
+      
+          setDelays(fetchedDelays);
+          forceUpdate();
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      };
+  
     const refreshData = async () => {
-        fetchData(); 
+      await fetchData();
     };
-
+  
     return (
-        <SafeAreaView style={styles.container}>
-             <ScrollView style={styles.scrollView}>
-            <View style={styles.container3}>
-                <View>
-                    <Text style={styles.textContainer}>Aktualne opóźnienia</Text>
-                    {departures && departures.map(departure => (
-                        <View key={departure.headsign} style={styles.departureContainer}>
-                            <Text style={styles.textContainer2}>Czas przyjazdu: {departure.estimatedTime}</Text>
-                            <Text style={styles.textContainer3}>Realny czas: {departure.theoreticalTime}</Text>
-                            <Text style={styles.textContainer3}>Brygada: {departure.vehicleService}</Text>
-                            <Text style={styles.textContainer4}>Kierunek: {departure.headsign}</Text>
-                            <Text style={styles.textContainer5}>Linia: {departure.routeId}</Text>
-                        </View>
-                    ))}
-                </View>
-                <TouchableOpacity onPress={refreshData} style={styles.button}>
-                    <Text style={styles.buttonText}>Odśwież dane</Text>
-                </TouchableOpacity>
+      <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.scrollView}>
+          <View style={styles.container3}>
+            <View>
+              <Text style={styles.textContainer}>Aktualne opóźnienia</Text>
+              {Array.isArray(delays) && delays.length > 0 ? (
+                delays.map((delay, index) => (
+                  <View key={index} style={styles.delayContainer}>
+                    <Text>Czas przyjazdu: {delay.estimatedTime}</Text>
+                    <Text>Realny czas: {delay.theoreticalTime}</Text>
+                    <Text>Brygada: {delay.vehicleService}</Text>
+                    <Text>Kierunek: {delay.headsign}</Text>
+                    <Text>Linia: {delay.routeId}</Text>
+                  </View>
+                ))
+              ) : (
+                <Text style={styles.loadingText}>Brak danych do wyświetlenia</Text>
+              )}
             </View>
-            </ScrollView>
-        </SafeAreaView>
+            <TouchableOpacity onPress={refreshData} style={styles.button}>
+              <Text style={styles.buttonText}>Odśwież dane</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
     );
-};
+  };
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#480CA8',
-        flex: 1,
-    },
-    textContainer: {
-        color: '#F72585',
-        fontSize: 40,
-    },
-    textContainer1: {
-        color: '#4CC9F0',
-        fontSize: 15,
-    },
-    textContainer2: {
-        color: '#4CC9F0',
-        fontSize: 15,
-    },
-    textContainer3: {
-        color: '#4CC9F0',
-        fontSize: 15,
-    },
-    textContainer4: {
-        color: '#4CC9F0',
-        fontSize: 15,
-    },
-    textContainer5: {
-        color: '#FFFFFF',
-        fontSize: 15,
-    },
-    button: {
-        color: '#FFFFFF',
-        borderColor: '#asdad',
-        borderWidth: 3,
-        borderRadius: 10,
-    },
-    container3: {
-        backgroundColor: '#480CA8',
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+  container: {
+    backgroundColor: '#480CA8',
+    flex: 1,
+  },
+  textContainer: {
+    color: '#F72585',
+    fontSize: 40,
+  },
+  delayContainer: {
+    borderColor: '#FFFFFF',
+    borderWidth: 2,
+    borderRadius: 8,
+    padding: 10,
+    marginVertical: 10,
+  },
+  button: {
+    backgroundColor: '#F72585',
+    borderColor: '#FFFFFF',
+    borderWidth: 2,
+    borderRadius: 8,
+    padding: 10,
+    marginVertical: 10,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  container3: {
+    backgroundColor: '#480CA8',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
 
 export default Home;
