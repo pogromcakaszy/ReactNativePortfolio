c790dacaf9bb84f7110789599f83ffad0916abfb
Kacper <pogromcakaszy@gmail.com>
2024-02-15 15:29:21 +0100

    1.0.0 Working Delay Display

--- Delayion/src/screens/Home.js
+++ Delayion/src/screens/Home.js
@@ -1,55 +1,60 @@
 import React, { useState, useEffect, useReducer } from 'react';
 import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  ScrollView,
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    ScrollView,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import RNPickerSelect from 'react-native-picker-select';
 import axios from 'axios';
+import stopId from './stopId';
 
 const apiKey = '5c188f0c-0b40-44ee-9ae1-96b818de8fa5';
-const stopId = 31430;
-const apiUrl = `http://ckan2.multimediagdansk.pl/delays?stopId=${stopId}`;
-
 const headers = {
-  'Authorization': `Bearer ${apiKey}`,
-  'Content-Type': 'application/json',
-};
-
-const Home = () => {
-    const [, forceUpdate] = useReducer(x => x + 1, 0);
-
+    'Authorization': `Bearer ${apiKey}`,
+    'Content-Type': 'application/json',
+  };
+  
+  const Home = () => {
+    const [, forceUpdate] = useReducer((x) => x + 1, 0);
+    const [selectedStop, setSelectedStop] = useState(Object.keys(stopId)[0]);
     const [delays, setDelays] = useState([]);
   
     useEffect(() => {
-      fetchData();
-      const intervalId = setInterval(fetchData, 20 * 1000);
+      fetchAllData();
+      const intervalId = setInterval(fetchAllData, 20 * 1000);
   
       return () => {
         clearInterval(intervalId);
       };
-    }, []);
+    }, [selectedStop]);
   
-    const fetchData = async () => {
-        try {
-          const response = await axios.get(apiUrl, { headers });
-          console.log('Status Code:', response.status);
-          console.log('Response Data:', response.data);
+    const fetchAllData = async () => {
+      const stopIdArray = stopId[selectedStop];
       
-          const fetchedDelays = response.data.delay || [];
-          console.log('Fetched Delays:', fetchedDelays);
-      
-          setDelays(fetchedDelays);
-          forceUpdate();
-        } catch (error) {
-          console.error('Error:', error);
+      if (stopIdArray) {
+        const allDelays = [];
+  
+        for (const stopId of stopIdArray) {
+          try {
+            const apiUrl = `http://ckan2.multimediagdansk.pl/delays?stopId=${stopId}`;
+            const response = await axios.get(apiUrl, { headers });
+            console.log('Status Code:', response.status);
+  
+            const fetchedDelays = response.data.delay || [];
+            console.log('Fetched Delays:', fetchedDelays);
+  
+            allDelays.push({ stopId, delays: fetchedDelays });
+          } catch (error) {
+            console.error('Error:', error);
+          }
         }
-      };
   
-    const refreshData = async () => {
-      await fetchData();
+        setDelays(allDelays);
+        forceUpdate();
+      }
     };
   
     return (
@@ -57,22 +62,32 @@ const Home = () => {
         <ScrollView style={styles.scrollView}>
           <View style={styles.container3}>
             <View>
+              <Text style={styles.textContainer}>Wybierz przystanek:</Text>
+              <RNPickerSelect
+                onValueChange={(value) => setSelectedStop(value)}
+                items={Object.keys(stopId).map((stopName) => ({
+                  label: stopName,
+                  value: stopName,
+                }))}
+              />
               <Text style={styles.textContainer}>Aktualne opóźnienia</Text>
-              {Array.isArray(delays) && delays.length > 0 ? (
-                delays.map((delay, index) => (
-                  <View key={index} style={styles.delayContainer}>
-                    <Text>Czas przyjazdu: {delay.estimatedTime}</Text>
-                    <Text>Realny czas: {delay.theoreticalTime}</Text>
-                    <Text>Brygada: {delay.vehicleService}</Text>
-                    <Text>Kierunek: {delay.headsign}</Text>
-                    <Text>Linia: {delay.routeId}</Text>
-                  </View>
-                ))
-              ) : (
-                <Text style={styles.loadingText}>Brak danych do wyświetlenia</Text>
-              )}
+              {delays.map(({ stopId, delays: stopDelays }, index) => (
+                <View key={index}>
+                  {Array.isArray(stopDelays) && stopDelays.length > 0 ? (
+                    stopDelays.map((delay, delayIndex) => (
+                      <View key={delayIndex} style={styles.delayContainer}>
+                        <Text>Realny czas: {delay.estimatedTime}</Text>
+                        <Text>Czas przyjazdu: {delay.theoreticalTime}</Text>
+                        <Text>Kierunek: {delay.headsign}</Text>
+                      </View>
+                    ))
+                  ) : (
+                    <Text style={styles.loadingText}>Brak danych do wyświetlenia</Text>
+                  )}
+                </View>
+              ))}
             </View>
-            <TouchableOpacity onPress={refreshData} style={styles.button}>
+            <TouchableOpacity onPress={fetchAllData} style={styles.button}>
               <Text style={styles.buttonText}>Odśwież dane</Text>
             </TouchableOpacity>
           </View>
@@ -80,42 +95,55 @@ const Home = () => {
       </SafeAreaView>
     );
   };
+  
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#480CA8',
-    flex: 1,
-  },
-  textContainer: {
-    color: '#F72585',
-    fontSize: 40,
-  },
-  delayContainer: {
-    borderColor: '#FFFFFF',
-    borderWidth: 2,
-    borderRadius: 8,
-    padding: 10,
-    marginVertical: 10,
-  },
-  button: {
-    backgroundColor: '#F72585',
-    borderColor: '#FFFFFF',
-    borderWidth: 2,
-    borderRadius: 8,
-    padding: 10,
-    marginVertical: 10,
-  },
-  buttonText: {
-    color: '#FFFFFF',
-    fontSize: 18,
-    textAlign: 'center',
-  },
-  container3: {
-    backgroundColor: '#480CA8',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+    container: {
+        backgroundColor: '#343A40',
+        flex: 1,
+    },
+    textContainer: {
+        color: '#FFFFFF',
+        fontSize: 40,
+    },
+    picker: {
+        borderColor: "#FFFFFF",
+        borderWidth: 10,
+    },
+    delayContainer: {
+        borderColor: '#343A40',
+        borderWidth: 2,
+        borderRadius: 8,
+        padding: 10,
+        marginVertical: 10,
+    },
+    button: {
+        backgroundColor: '#212529',
+        borderColor: '#303539',
+        borderWidth: 2,
+        borderRadius: 30,
+        padding: 10,
+        marginVertical: 10,
+    },
+    buttonText: {
+        color: '#F8F9FA',
+        fontSize: 18,
+        textAlign: 'center',
+    },
+    container3: {
+        backgroundColor: '#495057',
+        borderRadius: 20,
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    delayHeadSignText: {
+        color: '#FFFFFF',
+        fontSize: 30,
+    },
+    delayTextContainer : {
+
+    },
 });
 
 export default Home;
